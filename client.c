9322bbae308c27ee00894b58a68be4ce7fe5339f
Jimmy Alford <jimmy.alford316@gmail.com>
2024-12-26 21:48:15 -0500

    add all necessary functions to get server client working. it just doesn't work...

--- client.c
+++ client.c
@@ -1,21 +1,35 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    int status;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;  // will point to the results
+    int status, sockfd;
+    struct addrinfo hints, *res;
 
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
     hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
 
     // get ready to connect
-    status = getaddrinfo("www.example.net", "3490", &hints, &servinfo);
+    status = getaddrinfo("www.example.net", "3490", &hints, &res);
 
     // servinfo now points to a linked list of 1 or more struct addrinfos
 
-    // etc.
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    ;
+    if ((status = connect(sockfd, res->ai_addr, res->ai_addrlen)) != 0) {
+        fprintf(stderr, "connect error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    char *msg = "Jimmy was here!";
+    int len;
+
+    len = strlen(msg);
+    send(sockfd, msg, len, 0);
+    close(sockfd);
 }
--- server.c
+++ server.c
@@ -6,19 +6,40 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    int status;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;  // will point to the results
+    int status, sockfd, new_fd;
+    socklen_t addr_size;
+    struct sockaddr_storage their_addr;
+    struct addrinfo hints, *res;
 
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
     hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-    if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+    if ((status = getaddrinfo(NULL, "3490", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    freeaddrinfo(servinfo); // free the linked-list
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if ((status = bind(sockfd, res->ai_addr, res->ai_addrlen)) != 0) {
+        fprintf(stderr, "bind error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    if ((status = listen(sockfd, 20)) != 0) {
+        fprintf(stderr, "listen error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    addr_size = sizeof their_addr;
+    if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) != 0) {
+        fprintf(stderr, "accept error: %s\n", gai_strerror(new_fd));
+        exit(1);
+    }
+
+    char buff[100];
+    recv(new_fd, buff, 99, 0);
+    printf("Message received: %s\n", buff);
+
+    freeaddrinfo(res); // free the linked-list
 }
